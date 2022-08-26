5872e7cb967bcfbe65247d859a7fdf2bb4611341
wjc74751 <85719815+wjc74751@users.noreply.github.com>
2022-08-26 21:46:29 +0800

    Add files via upload

---
+++ bilibiliFromBulletScreenToUID.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+
+CRCTableFiles = []
+
+for j in range(1,10):
+    CRCTableFiles.append([])
+    for k in range(10):
+        CRCTableFiles[j-1].append(open(f"FromBulletScreenToUID/CRCTable/{j}/{k}/CRCTable.txt","r"))
+
+cid=input("cid:")
+targetBulletScreen = input("bulletScreen:")
+url=f'https://comment.bilibili.com/{cid}.xml' #网络刷新
+headers={'User-Agent':'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Mobile Safari/537.36'}
+res=requests.get(url,headers=headers)
+res.encoding=res.apparent_encoding
+html = BeautifulSoup(res.text,'lxml')
+bulletScreens = html.find_all('d')
+for i in range(len(bulletScreens)):
+    if (targetBulletScreen == bulletScreens[i].text):
+        print("found target bulletScreen")
+        hashedUID =  int(eval("0x"+bulletScreens[i].attrs['p'].split(",")[6]))
+        for line in CRCTableFiles[int(str(hashedUID)[0])-1][int(str(hashedUID)[1])]:
+            if (int(line.split(",")[0]) == hashedUID):
+                UID = (line.split(",")[1])
+                print("found target UID")
+                print("UID: " + UID) 
+                break
+        
---
+++ makeCRC32Table.py
@@ -0,0 +1,26 @@
+from zlib import crc32
+import os
+
+CRCTableFiles = []
+
+os.mkdir("FromBulletScreenToUID/CRCTable") #二级检索
+for j in range(1,10):
+    os.mkdir(f"FromBulletScreenToUID/CRCTable/{j}")
+    CRCTableFiles.append([])
+    for k in range(10):
+        os.mkdir(f"FromBulletScreenToUID/CRCTable/{j}/{k}")
+        CRCTableFiles[j-1].append(open(f"FromBulletScreenToUID/CRCTable/{j}/{k}/CRCTable.txt","w"))
+    
+for i in range(0,100000000):
+    crc32Value = crc32(str(i).encode('utf-8'))
+    CRCTableFiles[int(str(crc32Value)[0])-1][int(str(crc32Value)[1])].write(str(crc32Value) + "," + str(i) + "\n")
+    finishRate = i/100000000
+    if (i%10000 == 0):
+        print(finishRate)
+
+
+
+            
+            
+
+     
