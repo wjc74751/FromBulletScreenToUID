ef466070c50dc038d80903aae79f05ee7b488c9e
wjc74751 <85719815+wjc74751@users.noreply.github.com>
2022-08-30 17:06:53 +0800

    improvement on storage

--- bilibiliFromBulletScreenToUID.py
+++ bilibiliFromBulletScreenToUID.py
@@ -3,11 +3,17 @@ from bs4 import BeautifulSoup
 
 CRCTableFiles = []
 
-for j in range(1,10):
+CRCTableFiles.append(open("FromBulletScreenToUID-main/CRCTable/OneLenCRCTable.txt", "r"))
+for j in range(1,16):
     CRCTableFiles.append([])
-    for k in range(10):
-        CRCTableFiles[j-1].append(open(f"FromBulletScreenToUID-main/CRCTable/{j}/{k}/CRCTable.txt","r"))
+    CRCTableFiles[j].append(open(f"FromBulletScreenToUID-main/CRCTable/{j}/TwoLenCRCTable.txt", "r"))
+    for k in range(16):
+        CRCTableFiles[j].append([])
+        CRCTableFiles[j][k+1].append(open(f"FromBulletScreenToUID-main/CRCTable/{j}/{k}/ThreeLenCRCTable.txt", "r"))
+        for l in range(16):
+            CRCTableFiles[j][k+1].append(open(f"FromBulletScreenToUID-main/CRCTable/{j}/{k}/{l}/CRCTable.txt","r"))
 
+        
 cid=input("cid:")
 targetBulletScreen = input("bulletScreen:")
 url=f'https://comment.bilibili.com/{cid}.xml' #网络刷新
@@ -19,11 +25,33 @@ bulletScreens = html.find_all('d')
 for i in range(len(bulletScreens)):
     if (targetBulletScreen == bulletScreens[i].text):
         print("found target bulletScreen")
-        hashedUID =  int(eval("0x"+bulletScreens[i].attrs['p'].split(",")[6]))
-        for line in CRCTableFiles[int(str(hashedUID)[0])-1][int(str(hashedUID)[1])]:
-            if (int(line.split(",")[0]) == hashedUID):
-                UID = (line.split(",")[1])
-                print("found target UID")
-                print("UID: " + UID) 
-                break
+        hashedUID =  bulletScreens[i].attrs['p'].split(",")[6] #stredHex
+        if (len(hashedUID) < 2):
+            for line in CRCTableFiles[0]:
+                if (line.split(",")[0] == hashedUID):
+                    UID = (line.split(",")[1])
+                    print("found target UID")
+                    print("UID: " + UID) 
+                    break
+        elif (len(hashedUID) < 3):
+            for line in CRCTableFiles[int(hashedUID[0],16)][0]:
+                if (line.split(",")[0] == hashedUID[1:]):
+                    UID = (line.split(",")[1])
+                    print("found target UID")
+                    print("UID: " + UID) 
+                    break
+        elif (len(hashedUID) < 4):
+            for line in CRCTableFiles[int(hashedUID[0],16)][int(hashedUID[1],16)+1][0]:
+                if (line.split(",")[0] == hashedUID[2:]):
+                    UID = (line.split(",")[1])
+                    print("found target UID")
+                    print("UID: " + UID) 
+                    break
+        else:
+            for line in CRCTableFiles[int(hashedUID[0],16)][int(hashedUID[1],16)+1][int(hashedUID[2],16)+1]:
+                if (line.split(",")[0] == hashedUID[3:]):
+                    UID = int(line.split(",")[1],16)
+                    print("found target UID")
+                    print("UID: " + str(UID)) #想借用我同学自我介绍时的一句话：我来这里就是想卷死在坐的各位 812803012
+                    break
         
--- makeCRC32Table.py
+++ makeCRC32Table.py
@@ -1,26 +1,35 @@
+import re
 from zlib import crc32
 import os
 
 CRCTableFiles = []
 
 os.mkdir("FromBulletScreenToUID-main/CRCTable") #二级检索
-for j in range(1,10):
+CRCTableFiles.append(open("FromBulletScreenToUID-main/CRCTable/OneLenCRCTable.txt", "w"))
+for j in range(1,16):
     os.mkdir(f"FromBulletScreenToUID-main/CRCTable/{j}")
     CRCTableFiles.append([])
-    for k in range(10):
+    CRCTableFiles[j].append(open(f"FromBulletScreenToUID-main/CRCTable/{j}/TwoLenCRCTable.txt", "w"))
+    for k in range(16):
         os.mkdir(f"FromBulletScreenToUID-main/CRCTable/{j}/{k}")
-        CRCTableFiles[j-1].append(open(f"FromBulletScreenToUID-main/CRCTable/{j}/{k}/CRCTable.txt","w"))
-    
-for i in range(0,100000000):
-    crc32Value = crc32(str(i).encode('utf-8'))
-    CRCTableFiles[int(str(crc32Value)[0])-1][int(str(crc32Value)[1])].write(str(crc32Value) + "," + str(i) + "\n")
-    finishRate = i/100000000
-    if (i%10000 == 0):
+        CRCTableFiles[j].append([])
+        CRCTableFiles[j][k+1].append(open(f"FromBulletScreenToUID-main/CRCTable/{j}/{k}/ThreeLenCRCTable.txt", "w"))
+        for l in range(16):
+            os.mkdir(f"FromBulletScreenToUID-main/CRCTable/{j}/{k}/{l}")
+            CRCTableFiles[j][k+1].append(open(f"FromBulletScreenToUID-main/CRCTable/{j}/{k}/{l}/CRCTable.txt","w"))
+print("Finished making folders")
+
+for i in range(0,1000000000):
+    crc32Value = hex(crc32(str(i).encode('utf-8')))[2:]
+    hexedI = hex(i)[2:]
+    if (len(crc32Value) < 2):
+        CRCTableFiles[0].write(crc32Value + "," + hexedI + "\n")
+    elif (len(crc32Value) < 3):
+        CRCTableFiles[int(crc32Value[0],16)][0].write(crc32Value[1:] + "," + hexedI + "\n")
+    elif (len(crc32Value) < 4):
+        CRCTableFiles[int(crc32Value[0],16)][int(crc32Value[1],16)+1][0].write(crc32Value[2:] + "," + hexedI + "\n")
+    else:
+        CRCTableFiles[int(crc32Value[0],16)][int(crc32Value[1],16)+1][int(crc32Value[2],16)+1].write(crc32Value[3:] + "," + hexedI + "\n")
+    finishRate = i/1000000000
+    if (i%100000 == 0):
         print(finishRate)
-
-
-
-            
-            
-
-     
